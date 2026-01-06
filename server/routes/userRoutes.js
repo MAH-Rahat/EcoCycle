4c829b7d1e1d73fb51ccb29c4ff2348596381929
tashdidnayeem <129997307+tashdidnayeem@users.noreply.github.com>
2026-01-06 07:05:44 +0600

    Delete server directory

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllCitizens, deleteUser } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.get('/all-citizens', getAllCitizens);
-router.delete('/:id', deleteUser);
-
-export default router;
